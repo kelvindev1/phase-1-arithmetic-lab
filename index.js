3afd9ca2fe8702d1ecd03ffa6274e0b3a140f661
kelvindev1 <kelvinmutugi336@gmail.com>
2024-03-21 15:45:32 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+var multiply;
+var num1 = 31;
+var num2 = 2;
+multiply= num1 * num2;
+var random = Math.floor(Math.random() * 1) + 1;
+var mod
+var num3 = 22 ;
+var num4 = 9 ;
+mod = num3 % num4 ;
+var max = Math.max(0,5,10,15,20);
